39a94bd1c703c08b22a48137db37d0c9eeff4407
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-06-23 21:00:17 -0400

    outlined js file for implementations that need to be done

---
+++ script.js
@@ -0,0 +1,34 @@
+let displayValue = 0;
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('number');
+const operators = document.querySelectorAll('operator');
+const clear = document.getElementById('clear');
+const sign = document.getElementById('sign');
+const percent = document.getElementById('percent');
+const zero = document.getElementById('zero');
+const decimal = document.getElementById('decimal');
+const equal = document.getElementById('equal');
+
+// Function implement numbers into display and update display
+
+
+// Function to implement clear display
+
+
+// Function to implement sign change (+/-)
+
+
+// Function to implement percent change
+
+
+// Function to implement decimal
+
+
+// Function to implement operators
+
+
+// Function to implement equals
+
+
+// Function that operates on two numbers and calculates operators
